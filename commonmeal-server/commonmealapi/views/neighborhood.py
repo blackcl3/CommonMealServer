e9eb11d145903a3c61b2b67311407ab848021aff
Christian Black <blackl3@gmail.com>
2023-01-23 14:40:44 -0600

    neighborhood pages now include user profiles

--- commonmeal-server/commonmealapi/views/neighborhood.py
+++ commonmeal-server/commonmealapi/views/neighborhood.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from rest_framework.decorators import action
-from commonmealapi.models import Neighborhood
+from commonmealapi.models import Neighborhood, User
 
 class NeighborhoodViewSet(ViewSet):
     
@@ -14,6 +14,8 @@ class NeighborhoodViewSet(ViewSet):
             response -- JSON serialized neighborhood
         """
         neighborhood = Neighborhood.objects.get(pk=pk)
+        neighborhood.uid = User.objects.filter(neighborhood=neighborhood)
+        neighborhood.uid.update()
         
         serializer = NeighborhoodSerializer(neighborhood)
         return Response(serializer.data)
@@ -30,8 +32,17 @@ class NeighborhoodViewSet(ViewSet):
         serializer = NeighborhoodSerializer(neighborhoods, many=True)
         return Response(serializer.data)
     
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'uid', 'public_profile', 'photo_url', 'address', 'neighborhood')
+    
 class NeighborhoodSerializer(serializers.ModelSerializer):
+    
+    uid = UserSerializer(many=True, read_only=True)
     
     class Meta:
         model = Neighborhood
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'uid')
+        depth = 2
